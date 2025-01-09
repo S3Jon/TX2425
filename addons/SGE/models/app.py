4cff6129e65f267ca37b3ea415c73c41ff97d531
Jon <sanchezeguiajon@gmail.com>
2025-01-09 13:14:05 +0100

    v3

--- addons/SGE/models/app.py
+++ addons/SGE/models/app.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import date
 
 class Project(models.Model):
     _name = 'sge.project'
@@ -7,18 +8,49 @@ class Project(models.Model):
 
     name = fields.Char(string='Nombre del Proyecto', tracking=True)
     state = fields.Selection([
-        ('draft', 'Borrador'),
+        ('cancelled', 'Cancelada'),
+        ('draft', 'Planificando'),
         ('in_progress', 'En Desarrollo'),
-        ('developing', 'Desarrollando'),
-        ('cancelled', 'Cancelada')
+        ('review', 'Revisando'),
+        ('done', 'Terminada'),
     ], string='Estado', default='draft', tracking=True)
     company_id = fields.Many2one('res.company', string='Empresa Cliente', tracking=True)
     communicator_id = fields.Many2one('hr.employee', string='Comunicador', tracking=True)
     has_ios_support = fields.Boolean(string='Soporte iOS', tracking=True)
-    employees_ids = fields.Many2many('hr.employee', string='Empleados', tracking=True)
+    project_hours_ids = fields.One2many('sge.project.hours', 'project_id', string='Horas Trabajadas')
     total_hours = fields.Float(string='Total de Horas', compute='_compute_total_hours', store=True, tracking=True)
+    start_date = fields.Date(string='Fecha de Inicio', default=fields.Date.context_today, readonly=True, tracking=True)
+    end_date = fields.Date(string='Fecha de Fin', readonly=True, tracking=True)
 
-    @api.depends('employees_ids.hours_spent')
+    @api.depends('project_hours_ids.hours')
     def _compute_total_hours(self):
         for project in self:
-            project.total_hours = sum(employee.hours_spent for employee in project.employees_ids)
+            project.total_hours = sum(hours.hours for hours in project.project_hours_ids)
+
+    @api.model
+    def create(self, vals):
+        if 'start_date' not in vals:
+            vals['start_date'] = fields.Date.context_today(self)
+        return super(Project, self).create(vals)
+
+    def action_set_cancelled(self):
+        if self.state not in ['cancelled', 'done']:
+            self.state = 'cancelled'
+            self.end_date = date.today()
+
+    def action_set_draft(self):
+        if self.state not in ['cancelled', 'done']:
+            self.state = 'draft'
+
+    def action_set_in_progress(self):
+        if self.state not in ['cancelled', 'done']:
+            self.state = 'in_progress'
+
+    def action_set_review(self):
+        if self.state not in ['cancelled', 'done']:
+            self.state = 'review'
+
+    def action_set_done(self):
+        if self.state not in ['cancelled', 'done']:
+            self.state = 'done'
+            self.end_date = date.today()
